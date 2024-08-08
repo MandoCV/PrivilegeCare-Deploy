2d05a82018f1059ef5fc2851d3fd2425b8ac07d1
Your Name <210237@utxicotepec.edu.com>
2024-08-08 12:46:41 -0600

    Pasando la Base de datos  a un entorno local para su maqetacion

--- backend/config/db.py
+++ backend/config/db.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://avnadmin:AVNS_YPv9gKHJKOhamUwUu3N@mysql-e331420-utxicotepec-f492.e.aivencloud.com:20907/defaultdb"
+# SQLALCHEMY_DATABASE_URL = "mysql://avnadmin:AVNS_YPv9gKHJKOhamUwUu3N@mysql-e331420-utxicotepec-f492.e.aivencloud.com:20907/defaultdb"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/baseFinal"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
