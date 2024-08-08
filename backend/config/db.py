089c8a5edecb3781039ffce93fc32ef4bf2a8d3c
Mando CV <210124@utxicotepec.edu.mx>
2024-08-07 23:40:39 -0600

    Proyecto Privilege Care

---
+++ backend/config/db.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "mysql://avnadmin:AVNS_YPv9gKHJKOhamUwUu3N@mysql-e331420-utxicotepec-f492.e.aivencloud.com:20907/defaultdb"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
