7ad9326b9f40a54d6ae20898c6c59adbabea04f3
JonaIbarra <ei9063584@gmail.com>
2024-08-09 14:07:24 -0600

    Se agrego el archivo departamentos.py

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from routes.userrol import userrol
 from routes.personal_medico import personal_medico
 from routes.puestos_departamentos import puesto_departamento
 from routes.puestos import puesto
+from routes.departamentos import departamentos
 from routes.areas_medicas import area_medica
 
 app=FastAPI(
@@ -16,7 +17,7 @@ app.include_router(user)
 app.include_router(person)
 app.include_router(rol)
 app.include_router(userrol)
-
+app.include_router(departamentos)
 app.include_router(personal_medico)
 app.include_router(puesto)
 app.include_router(puesto_departamento)
