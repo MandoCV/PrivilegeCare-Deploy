8b78082efb2ee1ce23d59389969216e48fd0da65
carlosM18-max <carloshernandez4199@gmail.com>
2024-08-09 13:03:04 -0600

    Correccion del app.py, borre mis rutas

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,6 @@ from routes.user import user
 from routes.person import person
 from routes.rol import rol
 from routes.userrol import userrol
-from routes.solicitudes import request
-from routes.tbc_organos import tbc_organos  
 
 app=FastAPI(
     title="HOSPITAL S.A. de C.V.",
@@ -14,6 +12,4 @@ app.include_router(user)
 app.include_router(person)
 app.include_router(rol)
 app.include_router(userrol)
-app.include_router(request)
-app.include_router(tbc_organos)
 print ("Hola bienvenido a mi backend")
