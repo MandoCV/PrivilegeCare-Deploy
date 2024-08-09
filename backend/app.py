8575066cd4b8be3478d2991d1c6802bc5347ba89
Arturo Aguilar Santos <210716@utxicotepec.edu.mx>
2024-08-09 13:42:35 -0600

    Implementacion de rutas de tablas de RegistrosM

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,9 @@ from routes.user import user
 from routes.person import person
 from routes.rol import rol
 from routes.userrol import userrol
-
+from routes.receta import receta
+from routes.citas import cita
+from routes.expediente import expediente
 app=FastAPI(
     title="HOSPITAL S.A. de C.V.",
     description="API para el almacenamiento de informacipn de un hospital"
@@ -12,4 +14,7 @@ app.include_router(user)
 app.include_router(person)
 app.include_router(rol)
 app.include_router(userrol)
+app.include_router(receta)
+app.include_router(cita)
+app.include_router(expediente)
 print ("Hola bienvenido a mi backend")
