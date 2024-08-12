590acd1f1c068034b33423e4f80d61b23b02bc4a
Daniela-AG15 <161864911+Daniela-AG15@users.noreply.github.com>
2024-08-12 14:35:42 -0600

    Update consumibles.py

--- backend/schemas/consumibles.py
+++ backend/schemas/consumibles.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
 from typing import Optional
+from pydantic import BaseModel
 from datetime import datetime
 import enum
 
 class EstatusEnum(str, enum.Enum):
     Activo = "Activo"
     Inactivo = "Inactivo"
-    En_Revision = "En Revisión"
+    En_Revisión = "En Revisión"
 
 class ConsumibleBase(BaseModel):
     Nombre: str
@@ -15,16 +15,20 @@ class ConsumibleBase(BaseModel):
     Tipo: str
     Departamento_ID: int
     Estatus: EstatusEnum = EstatusEnum.Activo
+    Fecha_Registro: Optional[datetime] = None
+    Fecha_Actualizacion: Optional[datetime] = None
     Observaciones: Optional[str] = None
     Espacio_Medico: str
 
 class ConsumibleCreate(ConsumibleBase):
     pass
 
-class ConsumibleUpdate(ConsumibleBase):
+class ConsumibleUpdate(BaseModel):
     pass
 
 class Consumible(ConsumibleBase):
     ID: int
+
     class Config:
         orm_mode = True
+
