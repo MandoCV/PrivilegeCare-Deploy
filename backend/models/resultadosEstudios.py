f539021b4721bdb1f4d912d042f615fb443b9ae7
MandoCV <112577507+MandoCV@users.noreply.github.com>
2024-08-19 14:58:26 -0600

    Update resultadosEstudios.py

--- backend/models/resultadosEstudios.py
+++ backend/models/resultadosEstudios.py
@@ -12,7 +12,7 @@ class ResultadosEstudios(Base):
     ID = Column(Integer, primary_key=True, autoincrement=True)
     #Paciente_ID = Column(Integer, ForeignKey("tbb_pacientes.Persona_ID"), nullable=False)
     #Personal_Medico_ID = Column(Integer, ForeignKey("tbb_personal_medico.Persona_ID"), nullable=False)
-    Estudio_ID = Column(Integer, ForeignKey("tbc_estudios.ID"), nullable=False)
+    #Estudio_ID = Column(Integer, ForeignKey("tbc_estudios.ID"), nullable=False)
     Folio = Column(String(50), nullable=False, unique=True)
     Resultados = Column(Text, nullable=False)
     Observaciones = Column(Text, nullable=False)
