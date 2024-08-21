35658dcc9c261822b9fb8e16ca45796bfc8bad25
MandoCV <112577507+MandoCV@users.noreply.github.com>
2024-08-20 23:43:42 -0600

    Update estudios.py

--- backend/routes/estudios.py
+++ backend/routes/estudios.py
@@ -21,26 +21,26 @@ def read_estudios(skip: int = 0, limit: int = 10, db: Session = Depends(get_db))
     db_estudios = crud.estudios.get_estudios(db=db, skip=skip, limit=limit)
     return db_estudios
 
-@estudios.get("/estudio/{id}", response_model=schemas.estudios.Estudios, tags=["Estudios"], dependencies=[Depends(Portador())])
+@estudios.get("/estudios/{id}", response_model=schemas.estudios.Estudios, tags=["Estudios"], dependencies=[Depends(Portador())])
 def read_estudio(id: int, db: Session = Depends(get_db)):
     db_estudio = crud.estudios.get_estudio(db=db, id=id)
     if db_estudio is None:
         raise HTTPException(status_code=404, detail="Estudio no encontrado")
     return db_estudio
 
-@estudios.post("/estudio/", response_model=schemas.estudios.Estudios, tags=["Estudios"])
+@estudios.post("/estudios/", response_model=schemas.estudios.Estudios, tags=["Estudios"])
 def create_estudio(estudio: schemas.estudios.EstudiosCreate, db: Session = Depends(get_db)):
     # Asumiendo que no hay un método para verificar duplicados de 'Estudio'
     return crud.estudios.create_estudio(db=db, estudio=estudio)
 
-@estudios.put("/estudio/{id}", response_model=schemas.estudios.Estudios, tags=["Estudios"], dependencies=[Depends(Portador())])
+@estudios.put("/estudios/{id}", response_model=schemas.estudios.Estudios, tags=["Estudios"], dependencies=[Depends(Portador())])
 def update_estudio(id: int, estudio: schemas.estudios.EstudiosUpdate, db: Session = Depends(get_db)):
     db_estudio = crud.estudios.update_estudio(db=db, id=id, estudio=estudio)
     if db_estudio is None:
         raise HTTPException(status_code=404, detail="Estudio no existe, no actualizado")
     return db_estudio
 
-@estudios.delete("/estudio/{id}", response_model=schemas.estudios.Estudios, tags=["Estudios"], dependencies=[Depends(Portador())])
+@estudios.delete("/estudios/{id}", response_model=schemas.estudios.Estudios, tags=["Estudios"], dependencies=[Depends(Portador())])
 def delete_estudio(id: int, db: Session = Depends(get_db)):
     db_estudio = crud.estudios.delete_estudio(db=db, id=id)
     if db_estudio is None:
