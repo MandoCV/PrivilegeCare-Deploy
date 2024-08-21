3131e8b436e70137d9dac18f7f4881f9ceba5397
YahirMaster777 <yahirperez141002@gmail.com>
2024-08-20 21:00:34 -0600

    Implementar autenticación con tokens en las rutas de Pediatria

--- backend/routes/Pediatria/vacunas.py
+++ backend/routes/Pediatria/vacunas.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, HTTPException, Depends, Query
 from sqlalchemy.orm import Session
-from cryptography import fernet
+from cryptography.fernet import Fernet
 import crud.Pediatria.vacunas, config.db, schemas.Pediatria.vacunas, models.Pediatria.vacunas
 from typing import List
+from jwt_config import solicita_token
+from portadortoken import Portador
+key=Fernet.generate_key()
+f = Fernet(key)
 
 vacuna = APIRouter()
 
@@ -15,12 +19,12 @@ def get_db():
     finally:
         db.close()
         
-@vacuna.get("/vacunas/", response_model=List[schemas.Pediatria.vacunas.Vacuna], tags=['Pediatria'])
+@vacuna.get("/vacunas/", response_model=List[schemas.Pediatria.vacunas.Vacuna], tags=['Pediatria'],dependencies=[Depends(Portador())])
 def read_vacunas(skip: int = Query(0, alias="page", ge=0), limit: int = Query(10, le=100), db: Session = Depends(get_db)):
     db_vacunas = crud.Pediatria.vacunas.get_vacunas(db=db, skip=skip*limit, limit=limit)
     return db_vacunas
 
-@vacuna.get("/vacunas/{id}/", response_model=List[schemas.Pediatria.vacunas.Vacuna], tags=['Pediatria'])
+@vacuna.get("/vacunas/{id}/", response_model=List[schemas.Pediatria.vacunas.Vacuna], tags=['Pediatria'],dependencies=[Depends(Portador())])
 def read_vacunas(id: int, skip: int = Query(0, alias="page", ge=0), limit: int = Query(10, le=100), db: Session = Depends(get_db)):
     # Se obtiene una lista de vacunas asociadas al nacimiento con el ID proporcionado
     db_vacunas = crud.Pediatria.vacunas.get_vacunas_by_nacimientos_id(db=db, nacimientos_id=id, skip=skip, limit=limit)
@@ -31,7 +35,7 @@ def read_vacunas(id: int, skip: int = Query(0, alias="page", ge=0), limit: int =
     return db_vacunas
 
 
-@vacuna.post("/vacunas/", response_model=schemas.Pediatria.vacunas.Vacuna, tags=["Pediatria"])
+@vacuna.post("/vacunas/", response_model=schemas.Pediatria.vacunas.Vacuna, tags=["Pediatria"],dependencies=[Depends(Portador())])
 def vacunas(vacuna: schemas.Pediatria.vacunas.VacunaCreate, db: Session = Depends(get_db)):
     db_vacuna_existente = crud.Pediatria.vacunas.get_vacunas_by_name_and_nacimientos_id(
         db, nombre=vacuna.vacuna_administrada, nacimientos_id=vacuna.nacimientos_id
